1bc7d1708b47ec9e07564dc1de6e2506bcdda038
Brayan Ruiz <55265523+BrayanRuiz@users.noreply.github.com>
2025-08-12 13:14:04 -0600

    [Finished] Sección-4 Primeros pasos en React

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,5 +1,15 @@
 import ItemCounter from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+    productName: string;
+    quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+    { productName: 'Nintendo Switch 2', quantity: 1 },
+    { productName: 'Pro Controller', quantity: 2 },
+    { productName: 'Super Smash', quantity: 5 },
+]
 
 export function FirstStepsApp() {
 
@@ -8,7 +18,15 @@ export function FirstStepsApp() {
         <>
             <h1>Carrito de compras</h1>
             
-            <ItemCounter />
+            {
+                itemsInCart.map(({ productName, quantity }) => (
+                    <ItemCounter key={productName} name={productName} quantity={quantity} />
+                ))
+            }
+
+            {/* <ItemCounter name="Nintendo Switch 2" quantity={1} />
+            <ItemCounter name="Pro Controller" quantity={2} />
+            <ItemCounter name="Super Smash" quantity={3} /> */}
         </>
     );
 };
--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import { useState } from "react";
 
-const ItemCounter = () => {
+interface Props {
+    name: string;
+    quantity?: number;
+} 
+
+const ItemCounter = ({name, quantity = 1}: Props) => {
+
+    const [ count, setCount ] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+
+    const handleSubtract = () => {
+        if (count === 1) return;
+        setCount(count - 1);
+    }
+
     return (
         <section
             style={{
                 display: 'flex',
                 alignItems: 'center',
                 gap: 10,
+                marginTop: 10,
             }}
         >
-            <button>+1</button>
-            <span>10</span>
-            <button>-1</button>
+            <span style={{ width: "150px", color: count === 1 ? 'red' : 'black' }}>{name}</span>
+            <button
+                onClick={handleAdd}
+            >+1</button>
+            <span>{count}</span>
+            <button
+                onClick={handleSubtract}
+            >-1</button>
         </section>
     )
 }
